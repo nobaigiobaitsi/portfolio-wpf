cb13cde5a4880622e35169138c60f0adec9b07b7
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-06-05 15:01:06 +0300

    Created the basic scaffolding

---
+++ WpfPortfolio/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WpfPortfolio.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentViewModel; // Holds the current ViewModel to be displayed in the UI
+
+        public ViewModelBase CurrentViewModel //Binds the UI to decide which View to show
+        {
+            get => _currentViewModel;
+            set => SetProperty(ref _currentViewModel, value); //Triggers UI updates when the ViewModel changes
+        }
+
+        public MainViewModel() //	Acts as the controller of the application
+        {
+            // The default?
+        }
+    }
+}
---
+++ WpfPortfolio/ViewModels/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+///OnPropertyChanged(...): Notifies the UI that a bound property changed, so the UI updates.
+///SetProperty(...): Safely updates a field and automatically raises PropertyChanged.
+
+namespace WpfPortfolio.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
